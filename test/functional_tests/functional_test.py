c7388e5bfea99be5449ff6f7e934352b027d24d0
Denis <denis.snagovskiy@list.ru>
2021-06-23 14:17:43 +0300

    Validation part of functional test

--- test/functional_tests/functional_test.py
+++ test/functional_tests/functional_test.py
@@ -1,7 +1,9 @@
-import pathlib
+import os
 import sys
 import unittest
-from unittest.mock import patch
+import datetime
+from pathlib import Path
+from unittest.mock import Mock, patch
 
 from test.base.classes import IntegratedTestCase 
 from test.base.pathcers import patch_sys_argv, patch_config_path
@@ -45,12 +47,29 @@ class FirstLaunchTest(IntegratedTestCase):
             self.assertEqual(expected_value, settings[setting])
 
         # It checks that default settings are valid and finds that:
+        #   * could build filename with given date format
+        filename = '2021-12-21.md'
+        destination = default_settings[DESTINATION].joinpath(filename)
+
+        mock_date = Mock(wraps=datetime.date)
+        mock_date.today.return_value = datetime.date(2012, 12, 21)
+        with patch('datetime.date', new=mock_date):
+            self.app.build_filename()
+
+        self.assertEqual(destination, self.app.destination())
+
         #   * destination file does not exist yet
         #   * destination path is writable
+        self.assertFalse(destination.is_file())
+        self.assertTrue(os.access(default_settings[DESTINATION], os.W_OK))
+
+        self.assertTrue(self.app.is_destination_valid())
 
         #   * template path file is exist and readable
+        self.assertTrue(Path(default_settings[TEMPLATE]).is_file())
+        self.assertTrue(os.access(default_settings[TEMPLATE], os.W_OK))
 
-        #   * date format is valid
+        self.assertEqual(expected, self.app.is_template_valid())
 
         # It creates markdown file on destination path
         #   * Files' filename is todays date in setting format
